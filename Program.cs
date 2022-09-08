4e39801ecb00592ad2c3e73fc3aa3a4d4a3e02de
kostya <kostya_pgs@mail>
2022-09-08 11:39:17 +0700

    1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// 
+
+string GetAreaRange(int areaNum) 
+
+{
+
+if (areaNum == 1) {
+return "x>0 y>0";
+}
+
+if (areaNum == 2) {
+return "x<0 y<0";
+}
+
+if (areaNum == 3) {
+return "x>0 y>0";
+}
+
+if (areaNum == 4) {
+return "x>0 y>0";
+}
+
+return "Введенный номер четверти больше 4-х";
+
+}
+
+Console.Write("Input area:  ");
+int areaNum = Convert.ToInt32(Console.ReadLine());
+
+string result = GetAreaRange(areaNum);
+Console.WriteLine(result);
+
+
